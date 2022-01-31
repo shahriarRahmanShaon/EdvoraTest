17088f2e722b1a9dbae1fc6e58cc47898a691192
shahriarRahmanShaon <shaon463@gmail.com>
2022-01-31 16:28:37 +0600

    Initial Commit

---
+++ EdvoraTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EdvoraTest
+//
+//  Created by sergio shaon on 31/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EdvoraTest/EdvoraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraTestApp.swift
+//  EdvoraTest
+//
+//  Created by sergio shaon on 31/1/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
